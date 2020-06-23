73e4874d1014650eb91d1b6fc4159813d0fa20ab
juddbaguio <juddmisaelbaguio@gmail.com>
2020-06-23 19:54:03 +0800

    Programming Assignment 2 DS Specialization Week 2

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,40 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  
+  setMatrix <- function (y) {
+    x <<- y
+    m <<- NULL
+  }
+  
+  getMatrix <- function () x
+  
+  setInverse <- function (y) {
+    m <<- solve(y)
+  }
+  
+  getInverse <- function() m
+  
+  list(setMatrix = setMatrix, getMatrix = getMatrix, setInverse = setInverse, getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+  
+  m <- x$getInverse()
+  
+  if(!is.null(m)) {
+    message('Getting cached data')
+    return(m)
+  }
+  
+  data <- x$getMatrix()
+  
+  m <- solve(data)
+  x$setInverse(m)
+  m
         ## Return a matrix that is the inverse of 'x'
 }
